6f389af1a32e9316aa05395ddbfb32480e433ac0
Keishiro Oyama <keishiro.oyama@kecy.me>
2023-04-30 09:41:16 +0900

    feat: add atoms/Icon component

---
+++ src/components/atoms/Icon/index.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Icon } from '.';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Atoms/Icon',
+  component: Icon,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const MicOn: Story = {
+  args: {
+    name: 'micOn',
+    size: 'md',
+    color: 'onPrimary.base',
+  },
+};
---
+++ src/components/atoms/Icon/index.tsx
@@ -0,0 +1,77 @@
+import { FC } from 'react';
+import { IconContext } from 'react-icons';
+import { 
+	BsChevronUp,
+	BsChevronDown,
+	BsChevronLeft,
+	BsChevronRight,
+	BsX,
+	BsList,
+	BsFillCheckCircleFill,
+	BsFillMicFill,
+	BsMicMuteFill,
+	BsKeyboard,
+	BsTrash,
+} from 'react-icons/bs';
+import { useTheme } from '@emotion/react';
+
+import { OnColorName, SizeName } from '../../../theme';
+
+export type IconName =
+	| 'chevronUp'
+	| 'chevronDown'
+	| 'chevronLeft'
+	| 'chevronRight'
+	| 'close'
+	| 'menu'
+	| 'doneFill'
+	| 'micOn'
+	| 'micOff'
+	| 'keyboard'
+	| 'trash';
+
+export type IconProps = {
+	name: IconName;
+  color: OnColorName;
+  size: SizeName;
+};
+
+export const Icon: FC<IconProps> = ({ name, color, size }) => {
+  const theme = useTheme();
+	const getIcon = (name: IconName) => {
+		switch (name) {
+			case 'chevronUp':
+				return <BsChevronUp />;
+			case 'chevronDown':
+				return <BsChevronDown />;
+			case 'chevronLeft':
+				return <BsChevronLeft />;
+			case 'chevronRight':
+				return <BsChevronRight />;
+			case 'close':
+				return <BsX />;
+			case 'menu':
+				return <BsList />;
+			case 'doneFill':
+				return <BsFillCheckCircleFill />;
+			case 'micOn':
+				return <BsFillMicFill />;
+			case 'micOff':
+				return <BsMicMuteFill />;
+			case 'keyboard':
+				return <BsKeyboard />;
+			case 'trash':
+				return <BsTrash />;
+			default:
+				return null;
+		}
+	};
+
+  return (
+    <IconContext.Provider
+      value={{ color: theme.colors[color], size: theme.iconSizes[size] }}
+    >
+			{getIcon(name)}
+    </IconContext.Provider>
+  );
+};
